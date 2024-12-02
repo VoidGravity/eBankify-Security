3ae6058c83503aafb3a9e8eae7058eb1721112fc
voidgravity <abdellahbardichwork@gmail.com>
2024-12-02 14:33:06 +0100

    updating applciation cingif an djwt service , fixing bugs

--- src/main/java/org/abdellah/ebankify1/security/JwtService.java
+++ src/main/java/org/abdellah/ebankify1/security/JwtService.java
@@ -17,7 +17,6 @@ import java.util.Map;
 import java.util.function.Function;
 
 
-
 @Service
 public class JwtService {
     @Value("${jwt.secret}")
@@ -50,6 +49,10 @@ public class JwtService {
         return extractClaim(token, Claims::getSubject);
     }
 
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
@@ -63,5 +66,23 @@ public class JwtService {
                 .getBody();
     }
 
-}
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
 
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public String refreshToken(String token) {
+        final Claims claims = extractAllClaims(token);
+        claims.setIssuedAt(new Date(System.currentTimeMillis()));
+        claims.setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME));
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
