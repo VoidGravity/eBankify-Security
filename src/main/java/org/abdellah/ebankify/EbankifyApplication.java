30bed3f29921e9fd7a3d88ee931be95cf8b2093e
voidgravity <abdellahbardichwork@gmail.com>
2024-12-02 09:35:39 +0100

    project init

---
+++ src/main/java/org/abdellah/ebankify/EbankifyApplication.java
@@ -0,0 +1,13 @@
+package org.abdellah.ebankify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbankifyApplication.class, args);
+    }
+
+}
