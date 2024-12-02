251ef31b83f88d88f7b20af64e37ed45df7a6552
voidgravity <abdellahbardichwork@gmail.com>
2024-12-02 11:15:59 +0100

    conteoller and repository  ,TODO : add layers needed in the controller

--- src/main/java/org/abdellah/ebankify1/controller/AuthController.java
+++ src/main/java/org/abdellah/ebankify1/controller/AuthController.java
@@ -1,10 +1,43 @@
 package org.abdellah.ebankify1.controller;
 
+import org.abdellah.ebankify1.dto.LoginRequest;
+import org.abdellah.ebankify1.security.JwtService;
+import org.abdellah.ebankify1.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
+    private final JwtService jwtService;
+    private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getEmail(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String token = jwtService.generateToken(userDetails);
+
+        return ResponseEntity.ok(Map.of("token", token));
+    }
+
+    @PostMapping("/admin/login")
+    public ResponseEntity<?> adminLogin(@RequestHeader("Authorization") String basicAuth) {
+        return ResponseEntity.ok().build();
+    }
 }
