2fed51ea66651f9af7ba9d5e4cec2839b89ff544
voidgravity <abdellahbardichwork@gmail.com>
2024-12-02 10:20:12 +0100

    handeling claims and extracting username

--- src/main/java/org/abdellah/ebankify1/service/JwtService.java
+++ src/main/java/org/abdellah/ebankify1/service/JwtService.java
@@ -2,6 +2,7 @@ package org.abdellah.ebankify1.service;
 
 
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -13,6 +14,7 @@ import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 
 
@@ -42,6 +44,23 @@ public class JwtService {
 
     private Key getSigningKey() {
         return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 
 }
